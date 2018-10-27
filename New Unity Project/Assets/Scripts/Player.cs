9b463e477f63986341d769020e2bc98498abe7b9
Juan-Albert <juanmartinezlopez96@gmail.com>
2018-10-27 19:32:00 +0200

    AimTest V1

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject stain;
+    public float speed = 5f;
+
+    private void Update()
+    {
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+
+        if(transform.rotation.eulerAngles.z <= 360 && transform.rotation.eulerAngles.z >= 275)
+        {
+            transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+        }
+        else if(transform.rotation.eulerAngles.z >= 180 && transform.rotation.eulerAngles.z < 275)
+        {
+            transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 180);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 steinPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            steinPos.z = 0;
+            Instantiate(stain, steinPos, Quaternion.identity);
+        }
+
+    }
+}
