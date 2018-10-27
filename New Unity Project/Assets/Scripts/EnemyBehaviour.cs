d5daea9f646bf115cfed458eebd3bf48cbf254b0
Laura Calle <laucalle09@gmail.com>
2018-10-27 23:08:18 +0200

    Enemy basic behaviuor and Spawner

---
+++ New Unity Project/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+    public Transform target;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+    private void Move()
+    {
+        Vector2 dir = new Vector2(target.position.x - transform.position.x,
+            target.position.y - transform.position.y);
+        dir.Normalize();
+        GetComponent<Rigidbody2D>().velocity = dir * speed;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    public GameObject[] enemies;
+    public Transform[] targets;
+    public Vector2 spawnValues;
+    float spawnWait;
+    public float maxSpawnWait;
+    public float minSpawnWait;
+    public int startWait;
+    public bool stop;
+
+    int randEnemy;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(WaitSpawner());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnWait = Random.Range(minSpawnWait, maxSpawnWait);
+	}
+    IEnumerator WaitSpawner() {
+        yield return new WaitForSeconds(startWait);
+
+        while (!stop) {
+            randEnemy = Random.Range(0, enemies.Length);
+            Vector3 SpawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x),
+                Random.Range(-spawnValues.y, spawnValues.y), 0);
+            GameObject enemy = Instantiate(enemies[randEnemy], SpawnPosition + transform.position, gameObject.transform.rotation) as GameObject;
+            enemy.GetComponent<EnemyBehaviour>().target = targets[Random.Range(0, targets.Length)];
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+
+}
