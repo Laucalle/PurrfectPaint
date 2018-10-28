6bd29da59697030a7ce69617646199ccfdad5304
Juan-Albert <juanmartinezlopez96@gmail.com>
2018-10-28 19:38:47 +0100

    Pause Menu

---
+++ New Unity Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject canvas;
+
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    private void Pause()
+    {
+        if(isPaused)
+        {
+            canvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            canvas.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        isPaused = !isPaused;
+    }
+
+    public void Resume()
+    {
+        Pause();
+    }
+}
