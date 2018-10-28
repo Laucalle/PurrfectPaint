c0b10e6c901a7769c08583e3e7ccc500aca0b314
Juan-Albert <juanmartinezlopez96@gmail.com>
2018-10-28 12:31:50 +0100

    AimBot V2

--- New Unity Project/Assets/Scripts/Player.cs
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -7,9 +7,13 @@ public class Player : MonoBehaviour {
     public GameObject stain;
     public float speed = 5f;
 
+    public float coneAngle = 15;
+    public float distance = 7.5f;
+
     private void Update()
     {
         Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //Todo esto se puede resumir en un transform.LookAt pero sin el tiempo de retardo de seguimiento
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
@@ -25,10 +29,39 @@ public class Player : MonoBehaviour {
 
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 steinPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            steinPos.z = 0;
-            Instantiate(stain, steinPos, Quaternion.identity);
+            for (int i = 0; i < 50; i++)
+            {
+                float minAngle = this.transform.rotation.eulerAngles.z - coneAngle;
+                float maxAngle = this.transform.rotation.eulerAngles.z + coneAngle;
+                //Debug.Log("Angulo: " + this.transform.rotation.eulerAngles + " Min: " + minAngle + " Max: " + maxAngle);
+
+                float newAngle = RandomFromDistribution.RandomRangeNormalDistribution(minAngle, maxAngle, RandomFromDistribution.ConfidenceLevel_e._95);
+
+                if (newAngle <= 360 && newAngle >= 275)
+                {
+                    newAngle = 0;
+                }
+                else if (newAngle >= 180 && newAngle < 275)
+                {
+                    newAngle = 180;
+                }
+
+                newAngle = newAngle * Mathf.Deg2Rad;
+
+                float newDistance = RandomFromDistribution.RandomRangeExponential(0, distance, 1, RandomFromDistribution.Direction_e.Right);
+
+                float catetoX = Mathf.Cos(newAngle) * newDistance;
+                float catetoY = Mathf.Abs(Mathf.Sin(newAngle) * newDistance);
+                //Debug.Log("NewAngle: " + newAngle + " NewDistance: " + newDistance + " CatetoX: " + catetoX + " CatetoY: " + catetoY);
+
+                Vector3 newPoint = this.transform.position + new Vector3(catetoX, catetoY, 0);
+
+                Instantiate(stain, newPoint, Quaternion.identity);
+            }
+
         }
+
+
 
     }
 }
