33db33854c0970d6b9c42b3eec35018c89d0325d
Laura Calle <laucalle09@gmail.com>
2018-10-30 02:38:46 +0100

    screenshake

---
+++ New Unity Project/Assets/Scripts/screenshake.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenshake : MonoBehaviour {
+
+    public Camera mainCam;
+
+    float shakeAmount = 0;
+	
+	void Awake () {
+        if (mainCam == null) {
+            mainCam = Camera.main;
+        }
+	}
+
+    public void Shake(float amt, float lenght) {
+        shakeAmount = amt;
+        InvokeRepeating("DoShake", 0, 0.1f);
+        Invoke("StopShake", lenght);
+    }
+
+    void DoShake() {
+        if (shakeAmount > 0) {
+            Vector3 camPos = mainCam.transform.position;
+            float offsetX = Random.value * shakeAmount * 2 - shakeAmount;
+            float offsetY = Random.value * shakeAmount * 2 - shakeAmount;
+            camPos.x += offsetX;
+            camPos.y += offsetY;
+
+            mainCam.transform.position = camPos;
+        }
+    }
+    void StopShake() {
+        CancelInvoke("DoShake");
+        mainCam.transform.localPosition = Vector3.zero;
+    }
+}
